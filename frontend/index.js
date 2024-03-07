b16532becadbc0df3a6c58b571afc634c4ebb9c9
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-03-07 12:58:33 -0600

    WIP

--- frontend/index.js
+++ frontend/index.js
@@ -1,9 +1,41 @@
+// const { default: axios } = require("axios")
+
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
+  
+  const learnersAxios = axios.get('http://localhost:3003/api/learners')
+  console.log(learnersAxios);
+  const learners = learnersAxios.data;
+  console.log(learners);
+
+  const mentorsAxios = axios.get('http://localhost:3003/api/mentors')
+  const mentors = mentorsAxios.data;
+
+  const info = document.querySelector('.info');
+  info.textContent = "No learner is selected";
+
+  let result = [];
+
+  learners.forEach(learner => {
+    const response = {
+      ...learner,
+      mentors: learner.mentors.map(mID => {
+        const mentor = mentors.find(mentor2 => {
+          mentor2.id == mID;
+        })
+        return mentor.firstName + " " + mentor.lastName
+      })
+    }
+    result.push(response);
+  })
+  result.forEach(learner => {
+    
+  })
+  
 
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
+  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear - 1}`
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }

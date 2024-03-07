affcbc9b319d45702b5642b1d5b927966273b00b
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-03-07 14:21:12 -0600

    Wip 2

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,11 @@
-// const { default: axios } = require("axios")
 
 async function sprintChallenge5() { // Note the async keyword, in case you wish to use `await` inside sprintChallenge5
   // 👇 WORK WORK BELOW THIS LINE 👇
-  
-  const learnersAxios = axios.get('http://localhost:3003/api/learners')
-  console.log(learnersAxios);
+  try {
+  const learnersAxios = await axios.get('http://localhost:3003/api/learners')
   const learners = learnersAxios.data;
-  console.log(learners);
 
-  const mentorsAxios = axios.get('http://localhost:3003/api/mentors')
+  const mentorsAxios = await axios.get('http://localhost:3003/api/mentors')
   const mentors = mentorsAxios.data;
 
   const info = document.querySelector('.info');
@@ -20,22 +17,69 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
     const response = {
       ...learner,
       mentors: learner.mentors.map(mID => {
-        const mentor = mentors.find(mentor2 => {
-          mentor2.id == mID;
-        })
+        const mentor = mentors.find(mentor2 =>
+          mentor2.id === mID);
         return mentor.firstName + " " + mentor.lastName
       })
     }
     result.push(response);
-  })
+  });
+
   result.forEach(learner => {
-    
+    const divCont = document.createElement('div');
+    divCont.classList.add("cards");
+    document.body.appendChild(divCont);
+
+    const div = document.createElement('div');
+    div.classList.add('card');
+    divCont.appendChild(div);
+
+    const h3 = document.createElement('h3');
+    h3.textContent = `${learner.fullName}`
+    div.appendChild(h3);
+
+    const divInner = document.createElement("div");
+    divInner.textContent = `${learner.email}`;
+    div.appendChild(divInner);
+
+    const h4 = document.createElement("h4");
+    h4.classList.add("closed");
+    h4.textContent = 'Mentors'
+    div.appendChild(h4);
+
+    const unli = document.createElement('ul');
+    div.appendChild(unli);
+
+    learner.mentors.forEach(mentorName => {
+      const mentorLiItem = document.createElement('li');
+      mentorLiItem.textContent = mentorName;
+      unli.appendChild(mentorLiItem);
+    })
+
+    h4.addEventListener("click", () => {
+      h4.classList.toggle("closed");
+      h4.classList.toggle("open");
+    })
+
+    div.addEventListener("click", (evt) => {
+      div.classList.toggle('selected')
+      if (evt.currentTarget === div) {
+        h3.textContent = `${learner.fullName}, ID ${learner.id}`;
+        info.textContent = `The selected learner is ${learner.fullName}`
+      } else if (!evt.currentTarget === div){
+        h3.textContent = `${learner.fullName}`
+        info.textContent = "No learner is selected";
+      }
+    })
   })
   
 
   const footer = document.querySelector('footer')
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear - 1}`
+} catch (error) {
+  console.error(error);
+}
 
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }

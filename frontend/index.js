ba6e3100c665df202eb8367fdb40179027a8de50
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-03-08 15:23:00 -0600

    WIP 3

--- frontend/index.js
+++ frontend/index.js
@@ -12,78 +12,113 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   info.textContent = "No learner is selected";
 
   let result = [];
-
+  
   learners.forEach(learner => {
     const response = {
       ...learner,
       mentors: learner.mentors.map(mID => {
         const mentor = mentors.find(mentor2 =>
           mentor2.id === mID);
-        return mentor.firstName + " " + mentor.lastName
-      })
-    }
-    result.push(response);
-  });
-
-  result.forEach(learner => {
-    const divCont = document.createElement('div');
-    divCont.classList.add("cards");
-    document.body.appendChild(divCont);
+          return mentor.firstName + " " + mentor.lastName
+        })
+      }
+      result.push(response);
+    });
+    
+    
+    result.forEach(learner => {
+    const divCont = document.querySelector(".cards")
+    divCont.append();
 
     const div = document.createElement('div');
     div.classList.add('card');
     divCont.appendChild(div);
-
+    
     const h3 = document.createElement('h3');
     h3.textContent = `${learner.fullName}`
     div.appendChild(h3);
-
+    
     const divInner = document.createElement("div");
     divInner.textContent = `${learner.email}`;
     div.appendChild(divInner);
-
+    
     const h4 = document.createElement("h4");
     h4.classList.add("closed");
     h4.textContent = 'Mentors'
     div.appendChild(h4);
-
+    
     const unli = document.createElement('ul');
     div.appendChild(unli);
-
+    
     learner.mentors.forEach(mentorName => {
       const mentorLiItem = document.createElement('li');
       mentorLiItem.textContent = mentorName;
       unli.appendChild(mentorLiItem);
     })
-
+    
+    let checkSelect = document.querySelectorAll('divCont');
+    
+    let selected = [];
+    
+    checkSelect.forEach((check) => {
+      check.addEventListener('change', () => {
+        if (check.checked) {
+          checkSelect.forEach((otherCheck) => {
+            if (otherCheck !== check) {
+              otherCheck.checked = false;
+            }
+          });
+          
+          selected = [check];
+        } else {
+          let idx = selected.indexOf(check);
+          if (idx !== -1) {
+            selected.splice(idx, 1);
+          }
+          
+          
+          if (selected.length === 0) {
+            info.textContent = "No learner is selected";
+            h3.textContent = `${learner.fullName}`
+          }
+          else if (selected.length <= 1) {
+            h3.textContent = `${learner.fullName}, ID ${learner.id}`;
+            info.textContent = `The selected learner is ${learner.fullName}`
+          }
+        }
+      })
+    })
+    
     h4.addEventListener("click", () => {
       h4.classList.toggle("closed");
       h4.classList.toggle("open");
     })
-
-    div.addEventListener("click", (evt) => {
+    
+    div.addEventListener("click", () => {
       div.classList.toggle('selected')
-      if (evt.currentTarget === div) {
-        h3.textContent = `${learner.fullName}, ID ${learner.id}`;
-        info.textContent = `The selected learner is ${learner.fullName}`
-      } else if (!evt.currentTarget === div){
-        h3.textContent = `${learner.fullName}`
-        info.textContent = "No learner is selected";
-      }
-    })
-  })
+      // if (!evt.currentTarget === divCont) {
+        //   h3.textContent = `${learner.fullName}`
+        //   info.textContent = "No learner is selected";
+        // } else {
+          //   h3.textContent = `${learner.fullName}, ID ${learner.id}`;
+          //   info.textContent = `The selected learner is ${learner.fullName}`
+          // }
+        })
+        
+      })
+      
+      
+      const footer = document.querySelector('footer')
+      const currentYear = new Date().getFullYear()
+      footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear - 1}`
+    } catch (error) {
+      console.error(error);
+    }
+    
+    // 👆 WORK WORK ABOVE THIS LINE 👆
+  }
   
-
-  const footer = document.querySelector('footer')
-  const currentYear = new Date().getFullYear()
-  footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear - 1}`
-} catch (error) {
-  console.error(error);
-}
-
-  // 👆 WORK WORK ABOVE THIS LINE 👆
-}
-
-// ❗ DO NOT CHANGE THE CODE  BELOW
-if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
-else sprintChallenge5()
+  // ❗ DO NOT CHANGE THE CODE  BELOW
+  if (typeof module !== 'undefined' && module.exports) module.exports = { sprintChallenge5 }
+  else sprintChallenge5()
+  
